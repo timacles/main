bb06f92d051a93ae72e15f498be03e67b2a8b05c
Tim A <timfosho@gmail.com>
2022-04-07 02:10:35 +0000

    new

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 export FLASK_APP=webserver.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1
 
-flask run -h 0.0.0.0 -p 80
+flask run -h 0.0.0.0 -p 80 
 
--- webserver.py
+++ webserver.py
@@ -9,15 +9,42 @@
 
 
 from flask import Flask
+from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return """<p>Hello, Shweta is a nice wife!</p>
-<p><p>Very pregnant though.
-</p></p>
-"""
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    tmlpt =  '''
+            <h1>
+                Hello welcome to my site!!! 
+                <p>
+                Type in a name for a secret message
+            </h1>
+           
+                <p><p>
+                Your browser is %s
+                <p>
+                %s
+           ''' 
+    site = tmlpt % (user_agent, request.remote_addr)        
+    return site
+
+
+@app.route("/resp/<name>")
+def index2(name):
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.route("/<name>")
+def hello_world(name):
+    return """  
+                <h1><p>Hello %s, Shweta is a nice wife!</p>
+                <p><p>Very pregnant though.
+           """ % name
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=80)
